36df1eab50657e2b81a71a2baad3f13aa779947f
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-06 15:17:53 +0700

    Update main.py

--- import subprocess  nama_file_input = input("Masukkan nama file yang ingin dienkripsi /main.py
+++ import subprocess  nama_file_input = input("Masukkan nama file yang ingin dienkripsi /main.py
@@ -13,18 +13,16 @@ tindakan = input("Pilih 1 atau 2: ")
 if tindakan == "1":
     nama_file_masukkan = input("Masukkan nama file yang ingin dienkripsi: ")
     nama_file_keluaran = input("Masukkan nama file keluaran: ")
-    passphrase = input("Masukkan kata sandi: ")
     perintah = f'openssl enc -aes-256-cbc -salt -in "{nama_file_masukkan}" -out "{nama_file_keluaran}"'
-    subprocess.run(perintah, shell=True, text=True, input=passphrase, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(perintah, shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("File telah dienkripsi.")
     exit(0)
 
 elif tindakan == "2":
     nama_file_masukkan = input("Masukkan nama file yang ingin didedekripsi: ")
     nama_file_keluaran = input("Masukkan nama file keluaran: ")
-    passphrase = input("Masukkan kata sandi: ")
     perintah = f'openssl enc -d -aes-256-cbc -in "{nama_file_masukkan}" -out "{nama_file_keluaran}"'
-    subprocess.run(perintah, shell=True, text=True, input=passphrase, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(perintah, shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("File telah didedekripsi.")
     exit(0)
 
