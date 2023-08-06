85d59f746075abadd004180d73ea4abafa72a29c
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-06 15:34:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,20 @@ tindakan = input("Pilih 1 atau 2: ")
 
 if tindakan == "1":
     nama_file_masukkan = input("Masukkan nama file yang ingin dienkripsi: ")
+
+    if not os.path.exists(nama_file_masukkan):
+        print(f"Kesalahan: File '{nama_file_masukkan}' tidak ditemukan.")
+        sys.exit(1)
+    
     nama_file_keluaran = input("Masukkan nama file keluaran: ")
+
+    if os.path.exists(nama_file_keluaran):
+        pilihan = input(f"File '{nama_file_keluaran}' sudah ada. Apakah Anda ingin menimpa file yang ada? (iya/tidak): ")
+    
+        if pilihan.lower() != 'iya':
+            print("Enkripsi dibatalkan.")
+            sys.exit(1)
+    
     perintah = f'openssl enc -aes-256-cbc -salt -in "{nama_file_masukkan}" -out "{nama_file_keluaran}"'
     subprocess.run(perintah, shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("\n[*] Sedang melakukan enkripsi...")
@@ -28,7 +41,20 @@ if tindakan == "1":
 
 elif tindakan == "2":
     nama_file_masukkan = input("Masukkan nama file yang ingin didedekripsi: ")
+
+    if not os.path.exists(nama_file_masukkan):
+        print(f"Kesalahan: File '{nama_file_masukkan}' tidak ditemukan.")
+        sys.exit(1)
+    
     nama_file_keluaran = input("Masukkan nama file keluaran: ")
+
+    if os.path.exists(nama_file_keluaran):
+        pilihan = input(f"File '{nama_file_keluaran}' sudah ada. Apakah Anda ingin menimpa file yang ada? (iya/tidak): ")
+    
+        if pilihan.lower() != 'iya':
+            print("Enkripsi dibatalkan.")
+            sys.exit(1)
+    
     perintah = f'openssl enc -d -aes-256-cbc -in "{nama_file_masukkan}" -out "{nama_file_keluaran}"'
     subprocess.run(perintah, shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("\n[*] Sedang melakukan dekripsi...")
