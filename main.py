4b961e3f0e3e47c88ed68a6d2f05d3e0b1c12e3b
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-06 15:24:30 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import subprocess
+
+print("""
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@ Program : Enkripsi dan deskripsi file   @
+@ Pembuat : Rofi [FII14]                  @
+@ GitHub  : https://github.com/FII14/eddf @
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+""")
+
+print("""
+Menu:
+
+[1] Enkripsi file
+[2] Deskripsi file
+[3] Keluar dari program
+""")
+
+tindakan = input("Pilih 1 atau 2: ")
+
+if tindakan == "1":
+    nama_file_masukkan = input("Masukkan nama file yang ingin dienkripsi: ")
+    nama_file_keluaran = input("Masukkan nama file keluaran: ")
+    perintah = f'openssl enc -aes-256-cbc -salt -in "{nama_file_masukkan}" -out "{nama_file_keluaran}"'
+    subprocess.run(perintah, shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print("File telah dienkripsi.")
+    exit(0)
+
+elif tindakan == "2":
+    nama_file_masukkan = input("Masukkan nama file yang ingin didedekripsi: ")
+    nama_file_keluaran = input("Masukkan nama file keluaran: ")
+    perintah = f'openssl enc -d -aes-256-cbc -in "{nama_file_masukkan}" -out "{nama_file_keluaran}"'
+    subprocess.run(perintah, shell=True, text=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print("File telah didedekripsi.")
+    exit(0)
+
+elif tindakan == "3":
+  exit(0)
+
+else:
+    print("Pilihan tidak valid.")
+    exit(1)
